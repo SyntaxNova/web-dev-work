fd71a0cf643105ef690a4c83dff9e2712060a89d
“Atharva <atharvacodes@gmail.com>
2023-11-15 15:19:59 +0530

    intitial commit

---
+++ php/Operators.php
@@ -0,0 +1,19 @@
+<?php
+    echo "Namaste vishwa " ;
+    //this is my first php code
+
+    echo "operators in php" ;
+    $a = 12 ;
+    $b = 9 ;
+    echo "Arithmatic operators" ;
+    echo "add" ;
+    echo $a + $b ;
+    echo "subtract" ;
+    echo $a - $b ;
+    echo "multiply" ;
+    echo $a * $b ;
+    echo "division" ;
+    echo $a / $b ;
+    echo "remainder" ;
+    echo $a % $b ;
+?>
