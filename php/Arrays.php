6d0f3a72e2c10c6824d7b909cac6e26417fd9578
“Atharva <atharvacodes@gmail.com>
2023-11-17 18:57:54 +0530

    initial commit

---
+++ php/Arrays.php
@@ -0,0 +1,13 @@
+<?php
+    $arr1 = array("abc" , "pqr" , "xyz" ) ;
+    
+    $arr2= array(
+        $a = array("abc1" , "pqr1" , "xyz1" ) , //numeric index array
+        $b = array("abc2" => 1000 , "pqr2"=> 1001 , "xyz2"=>1002 ) , //associative index array
+    )
+    //rewatch array and foreach loops video;
+
+
+
+?>
+
